feef24c710dd1af37c3417cc2850307579aff276
Aleksandr Riabov <sania.riabov@gmail.com>
2023-03-10 13:56:59 +0200

    Footer navlinks removed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,22 @@
 import { Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { EnvelopeAt, Github, Linkedin } from 'react-bootstrap-icons';
+
 
 const Footer = () => {
   return (
     <footer className="bg-custom-dark text-center text-lg-start footer">
-      <Container >
-        <div className='row '>
-          <Link to='/home' className='col-md-3 col-xs-12 footer-link'> Home</Link>
-          <Link to='/projects' className='col-md-3 col-xs-12 footer-link'> Projects</Link>
-          <Link to='/contact' className='col-md-3 col-xs-12 footer-link'> Contact</Link>
-          <Link to='/resume' className='col-md-3 col-xs-12 footer-link'> Resume</Link>
-        </div>
-      </Container>
+      <Container >     
       <div className="text-center color-custom-light p-3 footer-bottom" >
+        <div className='footer-social-links'>
+          <div> <a href="https://github.com/AleksandrRiabov" target="_blank" rel='noreferrer no-follow'> <Github /></a></div>
+          <div> <a href='https://www.linkedin.com/in/aleksandr-riabov/' target="_blank" rel='noreferrer no-follow'> <Linkedin /></a></div>
+          <div> <EnvelopeAt/> <span> sania.riabov@gmail.com</span></div>
+        </div>
+        
         © 2023 Copyright:
         <a className="text-light" href="https://github.com/AleksandrRiabov"> Aleksandr Riabov</a>
       </div>
-
-
+      </Container>
     </footer>
   )
 }
